1d85cc9fbeb93b05a744a895e83c0c762f7e59e3
Swati Rani <124399483+swati910567@users.noreply.github.com>
2024-09-12 23:17:33 +0530

    Update user.models.js

--- backend/models/user.models.js
+++ backend/models/user.models.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema=new mongoose.Schema({
     email:{
         type:String,
-        unique:true
+       required:true,
     },
     password:{
         type:String,
